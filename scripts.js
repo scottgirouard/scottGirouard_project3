90fa74ece9ff917c660a6396c79d6ffd56f9402e
Scott <girouard.scott@gmail.com>
2018-08-16 17:49:15 -0400

    ON THE RIGHT TRACK

--- scripts.js
+++ scripts.js
@@ -70,16 +70,39 @@ civilizations.answer = {
 // *******************************************************
 // Could I do it THIS way??? AUGUST 15th EVENING RESEARCH **********************************************************
 
-civilizations.tallyAnswers = function(){
+civilizations.tallyAnswers = function(array){
     for(let item in civilizations.answer) {
         $('section').append('civilizations.answer' + item.name);
 
-        // console.log(item.name);
+        console.log(item.name);
     }
 }
+civilizations.mostFrequentItem = function(array) {
+
+
+
+
+
+
+
+    // var counts = {}; //We are going to count occurrence of item here
+    // var compare = 0;  //We are going to compare using stored value
+    // var mostFrequentItem;  //We are going to store most frequent item
+    console.log(array);
+    this.tallyAnswers(array);
+    // console.log("success")
+
+ //find duplicates in array - push those duplicates to new array
+//compare array length to determine answer
+//index value of longest array will determine winning object
+
+
+
+};
 
 civilizations.getResults = function () {
     $('form').on('submit', function (e) {
+        // console.log("submitted");
         e.preventDefault();
         let answer = $('input[name=active]:checked').val();
         let answer2 = $('input[name=handy]:checked').val();
@@ -89,26 +112,43 @@ civilizations.getResults = function () {
         let answer6 = $('input[name=smart]:checked').val();
         let answer7 = $('input[name=power]:checked').val();
         
-        console.log(answer, answer2, answer3, answer4, answer5, answer6, answer7);
+        // console.log(answer, answer2, answer3, answer4, answer5, answer6, answer7);
 
         // ^ All answers are displayed in console
 
         // Now display them in an array:
-        let allAnswers = [answer, answer2, answer3, answer4, answer5, answer6, answer7]
+        let allAnswers = [answer, answer2, answer3, answer4, answer5, answer6, answer7];
 
 
-        let mostFrequentItem = function(array){
-            
-        }
+        console.log(allAnswers);
 
-        mostFrequentItem(allAnswers);
-
-        $('ul').empty();
-
-        civilizations.tallyAnswers(answer);
+        civilizations.mostFrequentItem(allAnswers);
     });
+};
+    //     for (var i = 0, i < allAnswers.length i++) {
+    //     var word = allAnswers[i];
 
-}
+    //     if (counts[word] === undefined) { //if count[word] doesn't exist
+    //         counts[word] = 1;    //set count[word] value to 1
+    //     } else {                  //if exists
+    //         counts[word] = counts[word] + 1; //increment existing value
+    //     }
+
+    //     if (counts[word] > compare) {  //counts[word] > 0(first time)
+    //         compare = counts[word];   //set compare to counts[word]
+    //         mostFrequent = allAnswers[i];  //set mostFrequent value
+
+    //     }
+    // };
+    
+
+
+//         $('ul').empty();
+
+//         civilizations.tallyAnswers(answer);
+//     });
+
+// }
 
 civilizations.init = function(){
     civilizations.getResults();
@@ -157,4 +197,4 @@ $(function() {
 
 // Step 1: Create a results section that is appended to the quiz page
 
-// Step 2: Correct object is displayed on the page via the .text, .html or .append
+// Step 2: Correct object is displayed on the page via the .text, .html  
