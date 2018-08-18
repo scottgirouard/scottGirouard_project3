eb8589437a276f405062aa4f264fa05817260a67
Scott <girouard.scott@gmail.com>
2018-08-18 17:39:34 -0400

    QUIZ FUCKIN WORKS DOPE420

--- scripts.js
+++ scripts.js
@@ -4,24 +4,25 @@ const civilizations = {}
 
 civilizations.answer = {
     a: {   
-        name : 'romans',
+        name : 'Romans',
         tally: 0
     },
     b: { 
-        name: 'greeks',
+        name: 'Greeks',
         tally: 0
     },
     c: {
-        name: 'persians',
+        name: 'Persians',
         tally: 0
     },
     d: {
-        name: 'chinese',
+        name: 'Chinese',
         tally: 0
     },
     e: {
-        name: 'mongols',
-        tally: 0
+        name: 'Mongols',
+        tally: 0,
+        content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, quod atque libero, placeat ex neque eveniet sit soluta accusamus nam eos ea incidunt laboriosam quia doloribus praesentium distinctio quidem mollitia minima quos illo deleniti eius. Quae necessitatibus odio, est ratione eligendi, sed maiores nesciunt libero adipisci beatae eius illum quasi.' 
     }
 };
 
@@ -81,28 +82,69 @@ $(function() {
 civilizations.displayWinner = function(finalTally){
     console.log(finalTally);
     let results = Object.entries(finalTally);
+    console.log(results);
 
     let cleanedUpResults = results.map(function(item){
           return item[1]
     });
 
     console.log(cleanedUpResults);
+    let allScores =[];
+    cleanedUpResults.forEach(function(scores){
+        allScores.push(scores.tally);
+
+    });
+    console.log(allScores);
+    let winner = Math.max(...allScores);
+    console.log(winner);
+
+    if(civilizations.answer.a.tally === winner) {
+        let yourCiv = civilizations.answer.a.name;
+        $('.resultBox').html(`<p class="civilization">Your civilization is ${yourCiv}</p>`);
+    } else if (civilizations.answer.b.tally === winner) {
+        let yourCiv = civilizations.answer.b.name;
+        $('.resultBox').html(`<p class="civilization">Your civilization is ${yourCiv}</p>`);
+    } else if (civilizations.answer.c.tally === winner) {
+        let yourCiv = civilizations.answer.c.name;
+        $('.resultBox').html(`<p class="civilization">Your civilization is ${yourCiv}</p>`);
+    } else if (civilizations.answer.d.tally === winner) {
+        let yourCiv = civilizations.answer.d.name;
+        $('.resultBox').html(`<p class="civilization">Your civilization is ${yourCiv}</p>`);
+    } else if (civilizations.answer.e.tally === winner) {
+        let yourCiv = civilizations.answer.e.name;
+        let yourContent = civilizations.answer.e.content;
+        $('.resultBox').html(`<p class="civilization">Your civilization is ${yourCiv}</p><p>${yourContent}</p>`);
+    } else null
+
+ //   $('.resultBox').html(`<p class="civilization">Your civilization is ${yourCiv}</p>`);
+       
+
+   
+        
+    
 
 }
 
 // Take the cleanedUpReults array and use the "sort" method to sort it by tally key in the object from highest to lowest
 
-keys = Object.keys(finalTally),
-i, len = keys.length;
+ //keys = Object.keys(finalTally),
+//i, len = keys.length;
 
 // in the brackets below, target the objects INSIDE the array
-//finalyTally[i].tally
+//finalTally[i].tally
 //end result of this function, is going to give you the object with the highest tally value
 //from there, append highest object to page
 
-keys.sort();
+// keys.sort();
 
-for (i = 0; i > finalTally.length; i++) {
-    k=keys[i];
-    alert(k + ':' + finalTally[k]);
-}
+// for (i = 0; i > finalTally.length; i++) {
+//     k=keys[i];
+//     alert(k + ':' + finalTally[k]);
+
+// var finalTally = $.map(items, function(i) {
+//     if (i < 2)
+//         return null;
+//     return i;    
+// });
+    
+// }
