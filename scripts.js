583cd37bf5381d41d70f7a8ac593da0bf17344ca
Scott <girouard.scott@gmail.com>
2018-08-19 01:46:03 -0400

    paragraphs all written

--- scripts.js
+++ scripts.js
@@ -6,12 +6,12 @@ civilizations.answer = {
     a: {   
         name : 'Romans',
         tally: 0,
-        content: 'Time Period: 550 BC - 465 BC<br><br>Based off your selections, you could have been a Roman due to your spirituality; the ancient Roman culture was full of myths, and often blurred the line between fact and fiction. Your (alleged) thirst for power would have also come in handy, as the Romans were the MOST POWERFUL ancient civilization, ruling over the biggest area of land in that era; all the countries surrounding the Mediterranean sea in present day were part of ancient Rome.' 
+        content: 'Time Period: 550 BC - 465 BC<br><br>Based off your selections, you could have been a Roman due to your spirituality; the ancient Roman culture was full of myths, and the foundation of ancient Rome is a legend in itself. Your (alleged) thirst for power would have also come in handy, as the Romans were the MOST POWERFUL ancient civilization overall, ruling over the largest area of land in that era; all the countries surrounding the Mediterranean sea in present day were part of ancient Rome.' 
     },
     b: { 
         name: 'Greeks',
         tally: 0,
-        content: 'YOU A GREEK!!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, quod atque libero, placeat ex neque eveniet sit soluta accusamus nam eos ea incidunt laboriosam quia doloribus praesentium distinctio quidem mollitia minima quos illo deleniti eius. Quae necessitatibus odio, est ratione eligendi, sed maiores nesciunt libero adipisci beatae eius illum quasi.' 
+        content: 'Time Period: 2700 BC - 1500 BC<br><br>Based off your selections, you could have been an ancient Greek either originating in Italy, Sicily, North Africa, or even as far west as France in the beginning... But of course, Greece itself was home to this incredibily influential civilization. Although the Greeks were not one of the oldest ancient civilizations, they are responsible for many staples of the modern world; You consider yourself to be a particularly political or athletic person, eh? You would have fit in well, as the ancient Greeks were not only responsible for the inception of democracy itself, but also the very first Olympic Games. They were also a scientifically advanced soceity, having created the base for modern geometry, biology and physics.' 
     },
     c: {
         name: 'Persians',
@@ -21,12 +21,12 @@ civilizations.answer = {
     d: {
         name: 'Chinese',
         tally: 0,
-        content: 'Test 420 TEST Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, quod atque libero, placeat ex neque eveniet sit soluta accusamus nam eos ea incidunt laboriosam quia doloribus praesentium distinctio quidem mollitia minima quos illo deleniti eius. Quae necessitatibus odio, est ratione eligendi, sed maiores nesciunt libero adipisci beatae eius illum quasi.' 
+        content: 'Time Period: 1600 BCE - 1046 BCE<br><br>Based off your selections, you could have been living in the ancient Chinese civilization, bearing witness to some incredible inventions that have enhanced the lives of humans since their inception; Important materials like paper and silk, useful tools like the compass and (in some cases) gunpowder & cannons, and last but not least, the cause of, and solution to, all of lifes problems; alcohol. Party on, China.' 
     },
     e: {
         name: 'Mongols',
         tally: 0,
-        content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, quod atque libero, placeat ex neque eveniet sit soluta accusamus nam eos ea incidunt laboriosam quia doloribus praesentium distinctio quidem mollitia minima quos illo deleniti eius. Quae necessitatibus odio, est ratione eligendi, sed maiores nesciunt libero adipisci beatae eius illum quasi.' 
+        content: 'Time Period: 1206-1368<br><br>Based off your selections, you could have been part of the famously dominant Mongol civilization; the worlds largest empire at the time of its rule, continuously defeating much more populated and powerful enemies than itself until its eventual demise. Founded by the warlord Genghis Khan (or Temujin as he was otherwise known as) along with two million other Mongols, defied all odds and proceeded to conquer most of the Middle East, China and Russia. They were a well-travlled civilization in that respect, and were as resilient as they were resourceful; they had large mobile armies, carrying their herds with them so they could sustain themselves off of horse blood in times of desperation. Sounds tasty... and very metal.'
     }
 };
 
